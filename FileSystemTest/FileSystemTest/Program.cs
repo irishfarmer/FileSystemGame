bbd6534086d8af54c4f925edeafde8fe46b67622
Phillip Artin <irishfarmer@gmail.com>
2014-05-23 14:19:44 -0500

    project test files

---
+++ FileSystemTest/FileSystemTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FileSystemTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fileCount = 0;
+            var d = new DirectoryInfo(@"c:\");
+            foreach (var file in d.GetDirectories())
+            {
+                Console.WriteLine("Root directory: " + file.Name);
+                var dn = new DirectoryInfo(d.Name + file.Name);
+                foreach (var newfile in dn.GetDirectories())
+                    Console.WriteLine("\t" + newfile.Name);
+            }
+            foreach (FileInfo fi in d.GetFiles())
+                Console.WriteLine(fi.Name);
+            Console.ReadKey();
+        }
+    }
+}
